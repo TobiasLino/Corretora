962a6b0bae32879f913e747653dd9889c1b5016c
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-01-25 21:09:57 -0300

    melhoria na estrutura de pastas

--- Solucao/lib/abstract/dates/mes.hpp
+++ Solucao/lib/abstract/dates/mes.hpp
@@ -1,13 +1,7 @@
 // Copyright 2020 Tobias
 #ifndef SOLUCAO_LIB_ABSTRACT_DATES_MES_HPP_
 #define SOLUCAO_LIB_ABSTRACT_DATES_MES_HPP_
-// Tipo enumerado para registro de data
-// Implementado em funções que exijam data
-// Exemplo:
-//  Cliente a, *ptr = nullptr;
-//  ptr = &;
-//  ptr->set_nascimento(24, OUTUBRO, 2001);
-//
+
 enum Mes {
   JANEIRO = 1, FEVEREIRO, MARCO, ABRIL, MAIO, JUNHO, JULHO, AGOSTO, SETEMBRO,
   OUTUBRO, NOVEMBRO, DEZEMBRO
--- Solucao/lib/address/address.cpp
+++ Solucao/lib/address/address.cpp
@@ -1,5 +1,53 @@
 // Copyright 2020 Tobias
 #include <iostream>
+#include <string>
 
 #include <address.hpp>
 
+Addres::Address() {
+  this->street_ = "";
+  this->neighborhood_ = "";
+  this->number_ = "";
+  this->city_ = "";
+  this->complement_ = "";
+  this->zip_ = "";
+}
+Address::~Address() {
+  std::cout << "DESTRUCTION: ADDRESS" << std::endl;
+}
+void set_street(std::string rua) {
+  this->street_ = rua;
+}
+void set_neighborhood(std::string bairro) {
+  this->neighborhood_ = bairro;
+}
+void set_number(std::string numero) {
+  this->number_ = numero;
+}
+void set_complement(std::string complemento) {
+  this->complement_ = complemento;
+}
+void set_city(std::string cidade) {
+  this->city_ = cidade;
+}
+void set_zip(std::string cep) {
+  this->zip_ = cep;
+}
+std::string get_street() __attribute__((pure)) {
+  return this->street_;
+}
+std::string get_neighborhood() __attribute__((pure)) {
+  return this->neighborhood_;
+}
+std::string get_number() __attribute__((pure)) {
+  return this->number_;
+}
+std::string get_complement() __attribute__((pure)) {
+  return this->complement_;
+}
+std::string get_city() __attribute__((pure)) {
+  return this->city_;
+}
+std::string get_zip() __attribute__((pure)) {
+  return this->zip_;
+}
